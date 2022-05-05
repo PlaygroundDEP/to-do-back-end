d1d9e47c39719944571a7961102385cedb43ea4a
Shakya Perera <ShakyaPerera111@gmail.com>
2022-05-05 21:19:39 +0530

    validate exceptions

---
+++ src/main/java/lk/ijse/dep8/todo/exception/ValidationException.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep8.todo.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+}
