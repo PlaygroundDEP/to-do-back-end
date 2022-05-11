83ac0440668cd5e59106c69cada3fe3525c87c2c
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-05-11 14:58:05 +0530

    fix an issue in login feature

--- src/main/java/lk/ijse/dep8/todo/api/LoginServlet.java
+++ src/main/java/lk/ijse/dep8/todo/api/LoginServlet.java
@@ -39,8 +39,8 @@ public class LoginServlet extends HttpServlet {
             Jsonb jsonb = JsonbBuilder.create();
             UserDTO user = jsonb.fromJson(req.getReader(), UserDTO.class);
 
-            if (user.getName() == null || !user.getName().matches("[A-Za-z ]+")) {
-                throw new ValidationException("Invalid User name");
+            if (user.getEmail() == null || !user.getEmail().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+                throw new ValidationException("Invalid Email");
             } else if (user.getPassword()==null || !user.getPassword().matches(".+")) {
                 throw new ValidationException("Invalid Password");
             }
